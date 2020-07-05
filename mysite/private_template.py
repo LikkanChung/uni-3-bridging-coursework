3ae10934da7ce9f8bdb9cc603200048893fb7f8a
Likkan Chung <llc721@student.bham.ac.uk>
2020-07-05 14:28:54 +0100

    Initial Django Project setup

---
+++ mysite/private_template.py
@@ -0,0 +1,8 @@
+"""
+This file is used to avoid pushing the private key to Git.
+Make a copy of this file to private.py
+Add to settings `from . import private`
+And make changes to any variables: `SECREY_KEY = private.SECRET_KEY`
+"""
+
+SECREY_KEY = ''
