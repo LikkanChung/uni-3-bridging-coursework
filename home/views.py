1521bdaa5b2346fdcda7d9f360517cf03520dd0a
Likkan Chung <llc721@student.bham.ac.uk>
2020-08-18 14:47:14 +0100

    Styling the home page

--- home/views.py
+++ home/views.py
@@ -6,5 +6,9 @@ from portfolio.models import Project
 
 # Create your views here.
 def home(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'home/home.html', {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    education = Education.objects.filter(published_date__lte=timezone.now()).order_by('end_date')
+    experience = Experience.objects.filter(published_date__lte=timezone.now()).order_by('end_date')
+    volunteering = Volunteering.objects.filter(published_date__lte=timezone.now()).order_by('end_date')
+    projects = Project.objects.filter(published_date__lte=timezone.now()).order_by('end_date')
+    return render(request, 'home/home.html', {'posts': posts, 'education': education, 'experience': experience, 'portfolio': projects})
