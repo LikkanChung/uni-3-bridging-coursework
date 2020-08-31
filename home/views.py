e8264235737147496b14301ba976e1a9362221c8
Likkan Chung <llc721@student.bham.ac.uk>
2020-08-31 19:56:32 +0100

    Item selection in CV section

--- home/views.py
+++ home/views.py
@@ -7,8 +7,8 @@ from portfolio.models import Project
 # Create your views here.
 def home(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    education = Education.objects.filter(published_date__lte=timezone.now()).order_by('end_date').first()
-    experience = Experience.objects.filter(published_date__lte=timezone.now()).order_by('end_date').first()
-    volunteering = Volunteering.objects.filter(published_date__lte=timezone.now()).order_by('end_date').first()
+    education = Education.objects.filter(published_date__lte=timezone.now()).order_by('-end_date').first()
+    experience = Experience.objects.filter(published_date__lte=timezone.now()).order_by('-end_date').first()
+    volunteering = Volunteering.objects.filter(published_date__lte=timezone.now()).order_by('-end_date').first()
     projects = Project.objects.filter(published_date__lte=timezone.now()).order_by('-end_date')
     return render(request, 'home/home.html', {'posts': posts, 'education': education, 'experience': experience, 'volunteering': volunteering, 'projects': projects})
